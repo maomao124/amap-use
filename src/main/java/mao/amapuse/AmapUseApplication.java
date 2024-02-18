7c22609c2814c7030318f872287334465df750a5
mao <1296193245@qq.com>
2024-02-18 16:53:55 +0800

    amap-use

---
+++ src/main/java/mao/amapuse/AmapUseApplication.java
@@ -0,0 +1,15 @@
+package mao.amapuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmapUseApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AmapUseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/amapuse/AmapUseApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.amapuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmapUseApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
